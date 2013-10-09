594a10783ee1340bce87592a2ab83a2e06ea5248
chao <wdc881203@163.com>
2013-10-09 10:21:24 +0800

    Signed-off-by: chao <wdc881203@163.com>

--- src/android/Test.java
+++ src/android/Test.java
@@ -6,35 +6,17 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-
 public class Test extends CordovaPlugin {
     public static final String ACTION_ADD_TEST_ENTRY = "addTestEntry";
     
-    @Override
+   @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        try {
-            if (ACTION_ADD_TEST_ENTRY.equals(action)) { 
-               /*JSONObject arg_object = args.getJSONObject(0);
-              
-                      Builder builder = new AlertDialog.Builder(this);
-			   builder.setTitle("test");
-			   builder.setMessage("test success");
-			   builder.setPositiveButton("OK", null);
-			   builder.show();   */  
-			   this.beep(10);
-               callbackContext.success();
-               return true;
-            }
-            callbackContext.error("Invalid action");
-            return false;
-        } catch(Exception e) {
-            System.err.println("Exception: " + e.getMessage());
-            callbackContext.error(e.getMessage());
-            return false;
-        } 
+    if (ACTION_ADD_TEST_ENTRY.equals(action)) {
+        this.beep(args.getLong(0));
+        callbackContext.success();
+        return true;
     }
+    return false;  // Returning false results in a "MethodNotFound" error.
+}
+}
 }
