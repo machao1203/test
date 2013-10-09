5621f742f3641174eb37fe79660e1b2c83ed3f8a
chao <wdc881203@163.com>
2013-10-09 10:23:37 +0800

    Signed-off-by: chao <wdc881203@163.com>

--- www/test.js
+++ www/test.js
@@ -1,6 +1,7 @@
 var test = {
     createEvent: function(successCallback, errorCallback) {
         cordova.exec(
+			console.log('test3333333333333');
             successCallback, // success callback function
             errorCallback, // error callback function
             'Test', // mapped to our native Java class called "CalendarPlugin"
