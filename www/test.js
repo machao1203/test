6c6c731a3dad398471657b7f5e3324c5e17d942e
chao <wdc881203@163.com>
2013-10-09 11:09:31 +0800

    Signed-off-by: chao <wdc881203@163.com>

--- www/test.js
+++ www/test.js
@@ -1,12 +1,12 @@
 var test = {
     createEvent: function(successCallback, errorCallback) {
-        cordova.exec(
-			console.log('test3333333333333');
+        console.log('test3333333333333');
+		cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'Test', // mapped to our native Java class called "CalendarPlugin"
             'addTestEntry' // with this action name
         ); 
     }
-};
+}
 module.exports = test;
