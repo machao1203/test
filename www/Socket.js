c40ba541db99bfd915a3493f06cf80c0fe58af74
chao <wdc881203@163.com>
2013-10-11 14:42:35 +0800

    Signed-off-by: chao <wdc881203@163.com>

--- www/Socket.js
+++ www/Socket.js
@@ -1,29 +1,16 @@
-/*var toast = {
-    createEvent: function(msg,successCallback, errorCallback) {
-        console.log('test3333333333333');
-		cordova.exec(
-            successCallback, // success callback function
-            errorCallback, // error callback function
-            'ToastTest', // service name  mapped to our native Java class called "CalendarPlugin"
-            'addToastEntry', // with this action name
-            [{
-            'msg':msg
-			}]
-        ); 
-    }
-}
-module.exports = toast;
-*/
 var exec = require('cordova/exec');
 module.exports ={
-	 /**
-     * Open a native alert dialog, with a customizable title and button text.
-     *
-     * @param {String} message              Message to print in the body of the alert
-     * @param {Function} completeCallback   The callback that is called when user clicks on a button.
-     * @param {String} title                Title of the alert dialog (default: Alert)
-     * @param {String} buttonLabel          Label of the close button (default: OK)
-     */
     socket_connect: function() {
+    	console.log('123456787654321');
         exec(null, null, "socket", "connect", []);
-    }};
+    },
+    socket_close: function() {
+    	console.log('socket close');
+        exec(null, null, "socket", "close", []);
+    },
+    socket_send:function(msg)
+    {
+        console.log('socket send');
+        exec(null, null, "socket", "send", [msg]);
+    }
+    };
